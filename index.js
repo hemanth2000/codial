04ece2f8cc9434f5eec9aaf1637883f3d268f521
hemanth2000 <33031429+hemanth2000@users.noreply.github.com>
2021-07-07 00:39:37 +0530

    added template engine

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port = 8000;
 const app = express();
 app.use("/", router);
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error running the server: ${err}`);
