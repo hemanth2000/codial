521821cbff4741ae3b17b4a47e13f0c418dded2c
hemanth2000 <33031429+hemanth2000@users.noreply.github.com>
2021-07-06 23:07:05 +0530

    Created first directory structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 8000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error running the server: ${err}`);
+    return;
+  }
+  console.log(`Server opened at: ${port}`);
+});
